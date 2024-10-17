41fcb0ec147158d9c0ebbbd0263a4d4e78c35b07
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2024-10-17 17:49:22 +0530

    Update Deleting_node_from_BST.cpp

--- Deleting_node_from_BST.cpp
+++ Deleting_node_from_BST.cpp
@@ -1,3 +1,89 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct node{
+//     int data;
+//     node* left;
+//     node* right;
+// };
+// struct node* insert(struct node* root,int element){
+//     if(root==nullptr){
+//         node* newnode=(node*)malloc(sizeof(node));
+//         newnode->data=element;
+//         root=newnode;
+//         return root;
+//     }
+//     if(element<root->data){
+//         root->left=insert(root->left,element);
+//     }
+//     if(element>root->data){
+//         root->right=insert(root->right,element);
+        
+//     }
+//     return root;
+// }
+// void inorder(node* root){
+//     if(root!=nullptr){
+//         inorder(root->left);
+//         cout<<root->data<<" ";
+//         inorder(root->right);
+//     }
+// }
+// node* minimumsuccessor(node* root){
+//     if(root==nullptr){
+//         return root;
+//     }
+//     return minimumsuccessor(root->left);
+// }
+// struct node* deletenode(node* root,int element){
+//     if(root==nullptr){
+//         cout<<"Element not found";
+//         return root;
+//     }
+//     if(element<root->data){
+//         root->left=deletenode(root->left,element);
+//     }
+//     else if(element>root->data){
+//         root->right=deletenode(root->right,element);
+//     }else{
+//         //if leaf node
+//         if(root->left==nullptr && root->right==nullptr){
+//             root=nullptr;
+//         }else if(root->left==nullptr){
+//             root=root->right;
+//         }else if(root->right==nullptr){
+//             root=root->left;
+//         }else{
+//             node* temp=minimumsuccessor(root->right);
+//             root->data=temp->data;
+//             root->right=deletenode(root->right,temp->data);
+//         }
+//     }
+//     return root;
+// }
+// int main(){
+//     node* root=nullptr;
+//     int n,element,num;
+//     cin>>n;
+//     for(int i=0;i<n;i++){
+//         cin>>element;
+//         root=insert(root,element);
+//     }
+//     cout<<"Enter element u want to delete: ";
+//     cin>>num;
+//     inorder(root);
+//     root=deletenode(root,num);
+//     cout<<endl<<"After deleting node"<<endl;
+//     inorder(root);
+    
+    
+// }
+
+
+
+/////////////////////////////////////////////Improved Code//////////////////////////////////////////
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,7 +92,8 @@ struct node{
     node* left;
     node* right;
 };
-struct node* insert(struct node* root,int element){
+
+node* insert(node* root,int element){
     if(root==nullptr){
         node* newnode=(node*)malloc(sizeof(node));
         newnode->data=element;
@@ -18,10 +105,10 @@ struct node* insert(struct node* root,int element){
     }
     if(element>root->data){
         root->right=insert(root->right,element);
-        
     }
     return root;
 }
+
 void inorder(node* root){
     if(root!=nullptr){
         inorder(root->left);
@@ -29,24 +116,24 @@ void inorder(node* root){
         inorder(root->right);
     }
 }
-node* minimumsuccessor(node* root){
+
+struct node* minimumsuccessor(node* root){
     if(root==nullptr){
         return root;
     }
     return minimumsuccessor(root->left);
 }
+bool ispresent=true;
 struct node* deletenode(node* root,int element){
     if(root==nullptr){
         cout<<"Element not found";
+        ispresent=false;
         return root;
-    }
-    if(element<root->data){
+    }else if(element<root->data){
         root->left=deletenode(root->left,element);
-    }
-    else if(element>root->data){
+    }else if(element>root->data){
         root->right=deletenode(root->right,element);
     }else{
-        //if leaf node
         if(root->left==nullptr && root->right==nullptr){
             root=nullptr;
         }else if(root->left==nullptr){
@@ -61,20 +148,26 @@ struct node* deletenode(node* root,int element){
     }
     return root;
 }
-int main(){
+int main() {
     node* root=nullptr;
-    int n,element,num;
+    int n;
+    cout<<"Enter number of elements in tree: ";
     cin>>n;
+    int element;
+    cout<<"Enter elements: "<<endl;
     for(int i=0;i<n;i++){
         cin>>element;
         root=insert(root,element);
     }
-    cout<<"Enter element u want to delete: ";
+    cout<<"Enter num to delete: ";
+    int num;
     cin>>num;
+    cout<<"Before deletion of node "<<endl;
     inorder(root);
+    cout<<endl;
     root=deletenode(root,num);
-    cout<<endl<<"After deleting node"<<endl;
-    inorder(root);
-    
-    
+    if(ispresent==true){
+        cout<<endl<<"After deletion of node"<<endl;
+        inorder(root);
+    }
 }
