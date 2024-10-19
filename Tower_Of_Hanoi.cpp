8a641c942c1d8bc6b860976e544dfc50109e11d3
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2024-10-19 20:04:30 +0530

    Update Tower_Of_Hanoi.cpp

--- Tower_Of_Hanoi.cpp
+++ Tower_Of_Hanoi.cpp
@@ -1,21 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void tower_of_hanoi(int n,char from,char to, char aux){
-    //Base case
+void tower_of_hanoi(int n,char source, char destination, char aux){
     if(n==0){
         return;
     }
-    tower_of_hanoi(n-1,from,aux,to);
-    //Printing Cases
-    cout<<"Move ring "<<n<<" from "<<from<<" to "<<to<<endl;
-    tower_of_hanoi(n-1,aux,to,from);
+    tower_of_hanoi(n-1,source,aux,destination);
+    cout<<"move ring "<<n<<" from "<<source<<" to "<<destination<<endl;
+    tower_of_hanoi(n-1,aux,destination,source);
 }
-int main(){
+int main() {
     int n;
     cin>>n;
     
     tower_of_hanoi(n,'A','C','B');
-    
     return 0;
 }
