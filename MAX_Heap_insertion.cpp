9d2c1e393e502d83bb709cc71951b250001dea58
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2024-10-27 22:44:31 +0530

    Update MAX_Heap_insertion.cpp

--- MAX_Heap_insertion.cpp
+++ MAX_Heap_insertion.cpp
@@ -1,81 +1,158 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct node {
+//     int data;
+//     node* left;
+//     node* right;
+// };
+
+// node* insert(int arr[], int size, int index) {
+//     if (index >= size) {
+//         return nullptr;
+//     }
+    
+//     node* root = new node;
+//     root->data = arr[index];
+//     root->left = insert(arr, size, 2 * index + 1);
+//     root->right = insert(arr, size, 2 * index + 2);
+
+//     return root;
+// }
+
+// void heapify(node* root) {
+//     if (root == nullptr) return;
+
+//     node* largest = root;
+
+//     if (root->left && root->left->data > largest->data) {
+//         largest = root->left;
+//     }
+
+//     if (root->right && root->right->data > largest->data) {
+//         largest = root->right;
+//     }
+
+//     if (largest != root) {
+//         swap(root->data, largest->data);
+//         heapify(largest);
+//     }
+// }
+
+// void inorder(node* root) {
+//     if (root != nullptr) {
+//         inorder(root->left);
+//         cout << root->data << " ";
+//         inorder(root->right);
+//     }
+// }
+
+// void buildMaxHeap(node* root) {
+//     if (root == nullptr) return;
+
+//     if (root->left) buildMaxHeap(root->left);
+//     if (root->right) buildMaxHeap(root->right);
+
+//     heapify(root);
+// }
+
+// int main() {
+//     node* root = nullptr;
+
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+
+//     cout << "Enter elements: " << endl;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     root = insert(arr, n, 0);
+
+//     buildMaxHeap(root);
+
+//     cout << "Inorder Traversal of the Max Heap: ";
+//     inorder(root);
+//     cout << endl;
+
+//     return 0;
+// }
+
+/////////////////////////////////////Updated and Easy Code //////////////////////////////////////////////////
+
+// You are using GCC
 #include <bits/stdc++.h>
 using namespace std;
 
-struct node {
+struct node{
     int data;
     node* left;
     node* right;
 };
 
-node* insert(int arr[], int size, int index) {
-    if (index >= size) {
+node* insert(int arr[],int size,int index){
+    if(index>=size){
         return nullptr;
     }
+    node* root=(node*)malloc(sizeof(node));
+    root->data=arr[index];
     
-    node* root = new node;
-    root->data = arr[index];
-    root->left = insert(arr, size, 2 * index + 1);
-    root->right = insert(arr, size, 2 * index + 2);
-
+    root->left=insert(arr,size,2*index+1);
+    root->right=insert(arr,size,2*index+2);
+    
     return root;
 }
 
-void heapify(node* root) {
-    if (root == nullptr) return;
-
-    node* largest = root;
-
-    if (root->left && root->left->data > largest->data) {
-        largest = root->left;
+void heapify(node* root){
+    if(root==nullptr){
+        return;
     }
-
-    if (root->right && root->right->data > largest->data) {
-        largest = root->right;
+    node* largest=root;
+    if(root->left && root->left->data>largest->data){
+        largest=root->left;
     }
-
-    if (largest != root) {
-        swap(root->data, largest->data);
+    if(root->right && root->right->data>largest->data){
+        largest=root->right;
+    }
+    if(largest!=root){
+        swap(root->data,largest->data);
         heapify(largest);
     }
 }
 
-void inorder(node* root) {
-    if (root != nullptr) {
+void buildMaxHeap(node* root){
+    if(root==nullptr){
+        return;
+    }
+    if(root->left!=nullptr){
+        buildMaxHeap(root->left);
+    }
+    if(root->right!=nullptr){
+        buildMaxHeap(root->right);
+    }
+    heapify(root);
+}
+void inorder(node* root){
+    if(root!=nullptr){
         inorder(root->left);
-        cout << root->data << " ";
+        cout<<root->data<<" ";
         inorder(root->right);
     }
 }
-
-void buildMaxHeap(node* root) {
-    if (root == nullptr) return;
-
-    if (root->left) buildMaxHeap(root->left);
-    if (root->right) buildMaxHeap(root->right);
-
-    heapify(root);
-}
-
 int main() {
-    node* root = nullptr;
-
+    node* root=nullptr;
     int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-
-    cout << "Enter elements: " << endl;
+    cout<<"Enter number of elements: ";
+    cin>>n;
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cout<<"Enter Elements"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-
-    root = insert(arr, n, 0);
-
+    root=insert(arr,n,0);
     buildMaxHeap(root);
-
-    cout << "Inorder Traversal of the Max Heap: ";
+    cout<<"Inorder Traversal: ";
     inorder(root);
-    cout << endl;
-
-    return 0;
 }
