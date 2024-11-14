8ad9a083543b513fd361947833f38dd6c97b4130
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2024-11-14 14:12:08 +0530

    Create MAX_HEAP_using_array.cpp

---
+++ MAX_HEAP_using_array.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(int arr[],int n,int i){
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+    
+    if(left<n && arr[left]>arr[largest]){
+        largest=left;
+    }
+    if(right<n && arr[right]>arr[largest]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+}
+void build(int arr[],int n){
+    for(int i=n/2-1;i>=0;i--){
+        heapify(arr,n,i);
+    }
+}
+void insert(int arr[],int *n,int value){
+    arr[*n]=value;
+    (*n)++;
+    int i=(*n)-1;
+    
+    while(i>0 && arr[(i-1)/2]<arr[i]){
+        swap(arr[i],arr[(i-1)/2]);
+        i=(i-1)/2;
+    }
+}
+void display(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<pow(arr[i],3)<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    int num;
+    int n=0;
+    cin>>num;
+    int element;
+    int arr[100];
+    for(int i=0;i<num;i++){
+        cin>>element;
+        insert(arr,&n,element);
+    }
+    build(arr,n);
+    display(arr,n);
+    
+    return 0;
+}
