34986d9feff0e547bea8cf02663d517dc68bd319
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2024-11-18 16:40:35 +0530

    Update Undirected_Graph_BFS.cpp

--- Undirected_Graph_BFS.cpp
+++ Undirected_Graph_BFS.cpp
@@ -1,42 +1,81 @@
-#include <iostream>
-#include <queue>
+// #include <iostream>
+// #include <queue>
+// using namespace std;
+
+// void bfs(int start, int adjMatrix[10][10], int V) {
+//     queue<int> q;
+//     bool visited[10] = {false};
+
+//     q.push(start);
+//     visited[start] = true;
+
+//     while (!q.empty()) {
+//         int node = q.front();
+//         q.pop();
+//         cout << node << " ";
+
+//         for (int i = 0; i < V; i++) {
+//             if (adjMatrix[node][i] == 1 && !visited[i]) {
+//                 q.push(i);
+//                 visited[i] = true;
+//             }
+//         }
+//     }
+// }
+
+// int main() {
+//     int V, E;
+//     cin >> V >> E;
+
+//     int adjMatrix[10][10] = {0};
+
+//     for (int i = 0; i < E; i++) {
+//         int u, v;
+//         cin >> u >> v;
+//         adjMatrix[u][v] = 1;
+//         adjMatrix[v][u] = 1;
+//     }
+
+//     bfs(0, adjMatrix, V);
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
 
-void bfs(int start, int adjMatrix[10][10], int V) {
-    queue<int> q;
-    bool visited[10] = {false};
-
+void BFS(int start,int matrix[10][10],int V){
+    queue<int>q;
+    bool visited[10]={false};
     q.push(start);
-    visited[start] = true;
-
-    while (!q.empty()) {
-        int node = q.front();
+    visited[start]=true;
+    
+    while(!q.empty()){
+        int node=q.front();
+        cout<<node<<" ";
         q.pop();
-        cout << node << " ";
-
-        for (int i = 0; i < V; i++) {
-            if (adjMatrix[node][i] == 1 && !visited[i]) {
+        for(int i=0;i<V;i++){
+            if(matrix[node][i]==1 && !visited[i]){
                 q.push(i);
-                visited[i] = true;
+                visited[i]=true;
             }
         }
+        
     }
 }
 
 int main() {
-    int V, E;
-    cin >> V >> E;
-
-    int adjMatrix[10][10] = {0};
-
-    for (int i = 0; i < E; i++) {
-        int u, v;
-        cin >> u >> v;
-        adjMatrix[u][v] = 1;
-        adjMatrix[v][u] = 1;
+    int V,E;
+    cin>>V>>E;
+    int matrix[10][10];
+    
+    int u,v;
+    for(int i=0;i<E;i++){
+        cin>>u>>v;
+        matrix[u][v]=1;
+        matrix[v][u]=1;
     }
-
-    bfs(0, adjMatrix, V);
-
-    return 0;
+    int start;
+    cin>>start;
+    BFS(start,matrix,V);
 }
