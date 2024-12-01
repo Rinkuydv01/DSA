3e8a7fa8dd760bc28bb7e8083f25ec24d99ea5b1
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2024-12-01 12:44:41 +0530

    Update OOPs_friend_function.cpp

--- OOPs_friend_function.cpp
+++ OOPs_friend_function.cpp
@@ -1,22 +1,56 @@
-// You are using GCC
+// // You are using GCC
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class student{
+//     int marks;
+//     public:
+//     void setmarks(int m){
+//         marks=m;
+//     }
+//     friend void afterIncrement(student s){
+//         cout<<"After Increment the marks are "<<s.marks+10;
+//     }
+// };
+// int main() {
+//     student st;
+//     int mrks;
+//     cin>>mrks;
+//     st.setmarks(mrks);
+//     afterIncrement(st);
+    
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 class student{
-    int marks;
+    int eng;
+    int mth;
+    int phy;
+    
     public:
-    void setmarks(int m){
-        marks=m;
+    student(){
+        eng=0;
+        mth=0;
+        phy=0;
     }
-    friend void afterIncrement(student s){
-        cout<<"After Increment the marks are "<<s.marks+10;
+    friend void setdata(student& s){
+        s.eng=95;
+        s.mth=96;
+        s.phy=97;
+    }
+    friend void getdata(student &s){
+        cout<<s.eng<<endl;
+        cout<<s.mth<<endl;
+        cout<<s.phy<<endl;
+        
     }
 };
+
 int main() {
-    student st;
-    int mrks;
-    cin>>mrks;
-    st.setmarks(mrks);
-    afterIncrement(st);
-    
+    student s1;
+    setdata(s1);
+    getdata(s1);
 }
