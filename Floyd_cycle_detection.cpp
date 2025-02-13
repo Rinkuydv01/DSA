a21f50dc2f8b764930437d598b85d29e9153e5db
Rinku Yadav <148479677+Rinkuydv01@users.noreply.github.com>
2025-02-13 21:33:57 +0530

    Update Floyd_cycle_detection.cpp

--- Floyd_cycle_detection.cpp
+++ Floyd_cycle_detection.cpp
@@ -14,3 +14,33 @@ bool floydCycleDetection(Node* head){
     }
     return false;
 }
+
+
+// Starting of LOOP in LL
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(fast==slow){
+                slow = head;
+
+                while(slow!=fast){
+                    slow = slow->next;
+                    fast = fast->next;
+
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
